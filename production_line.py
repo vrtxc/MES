5b42497e0054ed428e94af77ce89c9a6b80c1175
joell39 <joel.p.0705@gmail.com>
2024-08-30 08:48:17 +0200

    Add production_line getters

--- production_line.py
+++ production_line.py
@@ -2,7 +2,10 @@
 class ProductionLine:
     
     def __init__(self, name):
-        pass
+        self.__name: str = name
+        self.__order_number: int
+        self.__product_name: str
+        self.__quantity: int
     
 
     def create_order(self, order_number, product_name, quantity):
@@ -15,3 +18,16 @@ class ProductionLine:
 
     def finish_order(self, order_number):
         pass
+
+
+    def get_name(self):
+        return self.__name
+    
+    def get_order_number(self):
+        return self.__order_number
+    
+    def get_product_name(self):
+        return self.__product_name
+    
+    def get_quantity(self):
+        return self.__quantity
