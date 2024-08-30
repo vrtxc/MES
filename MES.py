b3873ea273269423baeb6662d62702d7cb5a5630
joell39 <joel.p.0705@gmail.com>
2024-08-30 08:32:12 +0200

    Add python classes

--- MES.py
+++ MES.py
@@ -1,1 +1,27 @@
-#MES Class
+
+class MES:
+    def __init__():
+        pass
+
+
+    def add_production_line(name):
+        pass
+
+
+    def get_product_line(name):
+        pass
+
+
+    def create_production_order(production_line_name, order_number, product_name, quantity):
+        pass
+
+
+    def start_production_order(production_line_name, order_number):
+        pass
+
+
+    def finish_production_order(production_line_name, order_number):
+        pass
+
+
+    
---
+++ mes_utils.py
@@ -0,0 +1,15 @@
+
+class mes_utils:
+
+    def __init__():
+        pass
+
+
+    @staticmethod 
+    def get_order_by_number(production_line, order_number): 
+        pass
+
+
+    @staticmethod 
+    def calculate_production_efficiency(order):
+        pass
