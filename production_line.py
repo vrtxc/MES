d496ec1d4387a181767a5442854e591806a92ed6
joell39 <joel.p.0705@gmail.com>
2024-09-27 08:37:17 +0200

    Add type hints to production_line

--- production_line.py
+++ production_line.py
@@ -2,26 +2,26 @@ from production_order import ProductionOrder
 
 class ProductionLine:
     
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.__name: str = name
-        self.__orders: list = []
+        self.__orders: list[ProductionOrder] = []
     
 
-    def add_order(self, order_number, product_name, quantity):
+    def add_order(self, order_number: int, product_name: str, quantity: int):
         self.__orders.append(
             ProductionOrder(order_number, product_name, quantity)
         )
 
 
-    def get_production_line_name(self):
+    def get_production_line_name(self) -> str:
         return self.__name
     
 
-    def get_orders(self):
+    def get_orders(self) -> list:
         return self.__orders
 
 
-    def start_order(self, order_number):
+    def start_order(self, order_number: int):
         order = self.get_order_by_order_number(order_number)
 
         if not order:
@@ -30,7 +30,7 @@ class ProductionLine:
         order.start()
 
 
-    def finish_order(self, order_number):
+    def finish_order(self, order_number: int):
         order = self.get_order_by_order_number(order_number)
 
         if not order:
@@ -39,7 +39,7 @@ class ProductionLine:
         order.finish()
     
 
-    def get_order_by_order_number(self, order_number):
+    def get_order_by_order_number(self, order_number: int) -> ProductionOrder:
         for order in self.__orders:
             if order.get_order_number() == order_number:
                 return order
